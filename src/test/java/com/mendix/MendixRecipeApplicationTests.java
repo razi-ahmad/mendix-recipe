20b2eb67a42da441aa552ab26740bf9d2609ebbf
razi.ahmad <razi.ahmad@justeattakeaway.com>
2023-05-28 14:43:33 +0200

    Added the test class

---
+++ src/test/java/com/mendix/MendixRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mendix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MendixRecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
